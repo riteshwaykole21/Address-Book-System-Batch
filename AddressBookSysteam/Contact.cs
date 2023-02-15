c278b5eef94d8ec3e47b305df148cd363f7a3306
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-02-15 08:30:08 +0530

    [Ritesh]add : Contact List

---
+++ AddressBookSysteam/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSysteam
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adderss { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string phoneNumber { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
