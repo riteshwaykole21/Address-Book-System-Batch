a50f943997c91e240b1ebf9905e93f0ed1ab7f8b
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-02-15 08:18:14 +0530

    [Ritesh]add : Welcome Meassage

---
+++ AddressBookSysteam/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSysteam
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome In Address Book Systeam ");
+            Console.ReadLine();
+        }
+    }
+}
